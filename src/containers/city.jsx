53f1bb6885dc9cb6e89a25265dccb2b44b995a91
Nicolas Martin <nicolas.artur.martin@gmail.com>
2019-09-03 11:53:39 +0200

    exercise solution for react wagon cities

--- src/containers/city.jsx
+++ src/containers/city.jsx
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
+import selectCity from '../actions/index';
 
+const City = (props) => {
+  return (
+    <li
+      className="list-group-item"
+      onClick={() => props.selectCity(props.city)}
+    >{props.city.name}</li>
+  );
+};
 
-class City extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return(
-      <div>
-        {this.props.name}
-      </div>
-    );
-  }
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectCity }, dispatch);
 }
 
-export default City;
+export default connect(null, mapDispatchToProps)(City);
--- src/containers/city_list.jsx
+++ src/containers/city_list.jsx
@@ -1,30 +1,34 @@
 import React, { Component } from 'react';
 import City from './city';
 
+import { connect } from 'react-redux'; 
 
 
 class CityList extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-        activeCity: ""
-      };
   }
 
   renderList = () => {
     return this.props.cities.map((city) => {
-      return <City key={city.name} activeCity={this.state.activeCity} name={city.name} address={city.address} slug={city.slug} />;
+      return <City key={city.name} city={city}/>;
     });
   }
-
+  
   render() {
     return(
-      <div className="cities">
+      <div className="list-group cities">
         {this.renderList()}
       </div>
     );
   }
+
+  
 }
 
-export default CityList;
+function mapStateToProps(state) { return {
+  cities: state.cities
+  };
+}
+export default connect(mapStateToProps)(CityList);
